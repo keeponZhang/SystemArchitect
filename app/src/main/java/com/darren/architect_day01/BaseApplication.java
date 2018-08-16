cf10128f5512011ef2c108c5b507fca1a373c616
keeponZhang <462789909@qq.com>
2018-08-16 23:03:19 +0800

    第一次提交

---
+++ app/src/main/java/com/darren/architect_day01/BaseApplication.java
@@ -0,0 +1,26 @@
+package com.darren.architect_day01;
+
+import android.app.Application;
+
+import com.darren.architect_day01.simple5.HttpUtils;
+import com.darren.architect_day01.simple5.OKHttpRequest;
+import com.darren.architect_day01.simple5.PreferencesUtil;
+
+import org.xutils.x;
+
+/**
+ * description:
+ * author: Darren on 2017/8/21 15:05
+ * email: 240336124@qq.com
+ * version: 1.0
+ */
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PreferencesUtil.getInstance().init(this);
+        x.Ext.init(this);
+        x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
+        HttpUtils.initHttpRequest(new OKHttpRequest());
+    }
+}
