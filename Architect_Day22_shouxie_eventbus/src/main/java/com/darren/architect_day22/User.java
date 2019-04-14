0bf88a9b6ecf1000b19cdb20046371454f0d41b4
keeponZhang <462789909@qq.com>
2019-04-15 02:15:46 +0800

    手写eventbus

---
+++ Architect_Day22_shouxie_eventbus/src/main/java/com/darren/architect_day22/User.java
@@ -0,0 +1,12 @@
+package com.darren.architect_day22;
+
+/**
+ * @创建者 keepon
+ * @创建时间 2019/4/15 0015 上午 12:09
+ * @描述 ${TODO}
+ * @版本 $$Rev$$
+ * @更新者 $$Author$$
+ * @更新时间 $$Date$$
+ */
+public class User {
+}
